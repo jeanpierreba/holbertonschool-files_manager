e93bfc03742203937c91254c8c05c6085b36dbb4
jeanpierreba <2713@holbertonschool.com>
2022-06-13 17:14:58 -0500

    Changed index

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,26 @@
 const express = require('express');
-const router = express.Router();
+const route = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
 const FilesController = require('../controllers/FilesController');
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
-router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
-router.get('/files/:id/data', FilesController.getFile);
+const router = (app) => {
+	app.use(express.json());
+	app.use('/', route);
+  
+	route.get('/status', (request, response) => AppController.getStatus(request, response));
+	route.get('/stats', (request, response) => AppController.getStats(request, response));
+	route.post('/users', (request, response) => UsersController.postNew(request, response));
+	route.get('/connect', (request, response) => AuthController.getConnect(request, response));
+	route.get('/disconnect', (request, response) => AuthController.getDisconnect(request, response));
+	route.get('/users/me', (request, response) => UsersController.getMe(request, response));
+	route.post('/files', (request, response) => FilesController.postUpload(request, response));
+	route.get('/files/:id', (request, response) => FilesController.getShow(request, response));
+	route.get('/files', (request, response) => FilesController.getIndex(request, response));
+	route.put('/files/:id/publish', (request, response) => FilesController.putPublish(request, response));
+	route.put('/files/:id/unpublish', (request, response) => FilesController.putUnpublish(request, response));
+	route.get('/files/:id/data', (request, response) => FilesController.getFile(request, response));
+  };
 
 module.exports = router;
