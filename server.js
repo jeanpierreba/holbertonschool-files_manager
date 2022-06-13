a90ff3b162a350f721e09c1ddc6364e82cb089c9
jeanpierreba <2713@holbertonschool.com>
2022-06-13 16:36:12 -0500

    Fixed errors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-import express from 'express';
-import controllerRouting from '.routes/index';
+const express = require('express');
+const router = require('./routes/index');
 
-const port = process.env.PORT || 5000;
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
-controllerRouting(app);
+app.use(router);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+export default app;
