639a9af0f8d09af146f01f05d94bf7220ee77a79
jeanpierreba <2713@holbertonschool.com>
2022-06-13 16:45:58 -0500

    Reverted some changed

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,18 +4,17 @@ import DBClient from '../utils/db';
 class AppController {
   static getStatus(req, res) {
     if (redisClient.isAlive() && DBClient.isAlive()) {
-		res.status(200).json({ redis: true, db: true}, 200);
+		res.status(200).json({ redis: true, db: true}, 200)
 	}
+    return res.status(200).send(data);
   }
 
   static async getStats(req, res) {
-      const users = await DBClient.ndUsers();
-	  const files = await DBClient.nbFiles();
     const data = {
-		users,
-		files,
+      users: await DBClient.ndUsers(),
+      files: await DBClient.nbFiles(),
     };
-    res.status(200).send(data);
+    return res.status(200).send(data);
   }
 }
 
