301f176adb8c00d258c9c692ce125128a8c4751b
jeanpierreba <2713@holbertonschool.com>
2022-06-13 16:10:29 -0500

    Fixed errors

--- utils/db.js
+++ utils/db.js
@@ -1,17 +1,28 @@
 import { MongoClient } from 'mongodb';
 
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+
 class DBClient {
   constructor() {
-    this.host = process.env.DB_HOST || 'localhost';
-    this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
-    this.client = new MongoClient(`mongodb://${this.host}:${this.port}`, { useUnifiedTopology: true });
-    this.client.connect();
-    this.db = this.client.db(this.database);
+    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+        if (!error) {
+            this.db = client.db(DB_DATABASE);
+            this.users = this.db.collection('users');
+            this.files = this.db.collection('files');
+        } else {
+            console.log(error.message);
+            this.db = false
+        }
+    });
   }
 
   isAlive() {
-    if (this.client.isConnected()) return true;
+    if (this.db) {
+        return true;
+    }
     return false;
   }
 
